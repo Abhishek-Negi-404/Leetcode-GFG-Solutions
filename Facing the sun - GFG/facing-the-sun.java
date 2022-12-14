d737d0f3d2282a6e5af64413f75c595269cd027c
Abhishek-Negi-404 <117743203+Abhishek-Negi-404@users.noreply.github.com>
2022-12-14 18:25:20 +0530

    Added solution - LeetHub

---
+++ Facing the sun - GFG/facing-the-sun.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] h = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                h[i] = Integer.parseInt(inputLine[i]);
+            }
+
+
+            int ans = new Solution().countBuildings(h, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int countBuildings(int h[], int n) {
+        int count = 1;// 1st building will always face sun
+        int maxHeight = h[0];// assuming highest at start
+        for(int i=1;i<n;i++)
+        {// all buildings greater height will face sun
+            if(h[i]>maxHeight) {
+                count++;
+                maxHeight = h[i];// updating max heigh
+            }
+        }
+        return count;
+    }
+}
