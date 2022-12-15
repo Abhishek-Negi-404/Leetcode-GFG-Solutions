ff4d1e7a5827507340fb385be8ebc56d6be1f267
Abhishek-Negi-404 <117743203+Abhishek-Negi-404@users.noreply.github.com>
2022-12-15 22:26:36 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (90.23%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,16 +11,23 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null || head.next==null){
+        if(head==null || head.next==null){// base cases
             return false;
         }
+        
+        // making two pointers slow and fast to traverse
         ListNode slow = head;
         ListNode fast = head;
+        
+        // if no. of nodes = even --> fast = null
+        // if no, of nodes = odd --> fast.next = null
         while(fast!=null && fast.next!=null)
         {
+            // moving slow at --> X & fast at --> 2X
             slow = slow.next;
             fast = fast.next.next;
 
+            // is the pointers meet ==> cyclic
             if(slow==fast)
             return true;
         }
