b771f9c2cea36349bb7e80a3db0dc4407ba56a24
Abhishek-Negi-404 <117743203+Abhishek-Negi-404@users.noreply.github.com>
2022-12-17 18:47:04 +0530

    Time: 2 ms (98.87%), Space: 42.8 MB (77.79%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);// dummy node to keep track of head
+        ListNode sum = dummy;
+        
+        // pointers for traversal
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        
+        int carry = 0;
+        while(p1 != null  &&  p2 != null)
+        {
+            int value = p1.val + p2.val + carry;
+            carry = value/10;
+            ListNode nn = new ListNode(value%10);
+            sum.next = nn;
+            sum = sum.next;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        
+        // if list1 has leftover Nodes
+        while(p1 != null){
+            int value = p1.val + carry;
+            carry = value/10;
+            ListNode nn = new ListNode(value%10);
+            sum.next = nn;
+            sum = sum.next;
+            p1 = p1.next;
+        }
+        
+        // if list2 has leftover Nodes
+        while(p2 != null){
+            int value = p2.val + carry;
+            carry = value/10;
+            ListNode nn = new ListNode(value%10);
+            sum.next = nn;
+            sum = sum.next;
+            p2 = p2.next;
+        }
+        
+        // if something is left in carry
+        if(carry > 0){
+            ListNode nn = new ListNode(carry);
+            sum.next = nn;
+        }
+        
+        return dummy.next;// returning head using dummy
+    }
+}
