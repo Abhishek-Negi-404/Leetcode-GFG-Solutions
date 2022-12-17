dffad0e241ec13dcb6b97ef9b0e88bc8f1e92316
Abhishek-Negi-404 <117743203+Abhishek-Negi-404@users.noreply.github.com>
2022-12-17 19:17:52 +0530

    Time: 2 ms (98.87%), Space: 42.2 MB (94.53%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -20,61 +20,57 @@ class Solution {
         int carry = 0;
 
 // Solution 1 =====================================================================================
-//         while(p1 != null  &&  p2 != null)// until one list ends
-//         {
-//             int value = p1.val + p2.val + carry;
-//             carry = value/10;
-//             ListNode nn = new ListNode(value%10);
-//             sum.next = nn;
-//             sum = sum.next;
-//             p1 = p1.next;
-//             p2 = p2.next;
-//         }
-        
-//         // if list1 has leftover Nodes
-//         while(p1 != null){
-//             int value = p1.val + carry;
-//             carry = value/10;
-//             ListNode nn = new ListNode(value%10);
-//             sum.next = nn;
-//             sum = sum.next;
-//             p1 = p1.next;
-//         }
-        
-//         // if list2 has leftover Nodes
-//         while(p2 != null){
-//             int value = p2.val + carry;
-//             carry = value/10;
-//             ListNode nn = new ListNode(value%10);
-//             sum.next = nn;
-//             sum = sum.next;
-//             p2 = p2.next;
-//         }
-        
-//         // if something is left in carry
-//         if(carry > 0){
-//             ListNode nn = new ListNode(carry);
-//             sum.next = nn;
-//         }
-        
-// Solution 2 ======================================================================================
-        // until both lists are traversed and no carry is left
-        while(p1 !=null  ||  p2 != null)
+        while(p1 != null  &&  p2 != null)// until one list ends
         {
-            int value = carry;
-            if(p1 != null){
-                value += p1.val;
-                p1 = p1.next;
-            }
-            if(p2 != null){
-                value += p2.val;
-                p2 = p2.next;
-            }
+            int value = p1.val + p2.val + carry;
             carry = value/10;
             ListNode nn = new ListNode(value%10);
             sum.next = nn;
             sum = sum.next;
+            p1 = p1.next;
+            p2 = p2.next;
         }
+        
+        // if list1 has leftover Nodes
+        while(p1 != null){
+            int value = p1.val + carry;
+            carry = value/10;
+            ListNode nn = new ListNode(value%10);
+            sum.next = nn;
+            sum = sum.next;
+            p1 = p1.next;
+        }
+        
+        // if list2 has leftover Nodes
+        while(p2 != null){
+            int value = p2.val + carry;
+            carry = value/10;
+            ListNode nn = new ListNode(value%10);
+            sum.next = nn;
+            sum = sum.next;
+            p2 = p2.next;
+        }
+        
+// Solution 2 ======================================================================================
+        // until both lists are traversed and no carry is left
+        // while(p1 !=null  ||  p2 != null)
+        // {
+        //     int value = carry;
+        //     if(p1 != null){
+        //         value += p1.val;
+        //         p1 = p1.next;
+        //     }
+        //     if(p2 != null){
+        //         value += p2.val;
+        //         p2 = p2.next;
+        //     }
+        //     carry = value/10;
+        //     ListNode nn = new ListNode(value%10);
+        //     sum.next = nn;
+        //     sum = sum.next;
+        // }
+        
+        // if something is left in carry
         if(carry > 0){
             ListNode nn = new ListNode(carry);
             sum.next = nn;
