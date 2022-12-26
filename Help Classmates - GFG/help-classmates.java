38a31fea7e4eb1d6d0fe55edee62434f1e785256
Abhishek-Negi-404 <117743203+Abhishek-Negi-404@users.noreply.github.com>
2022-12-26 18:57:26 +0530

    Added solution - LeetHub

--- Help Classmates - GFG/help-classmates.java
+++ Help Classmates - GFG/help-classmates.java
@@ -40,25 +40,25 @@ class Solution {
 	public static int[] help_classmate(int arr[], int n) 
 	{ 
 	    Stack<Integer> s = new Stack<>();
-		s.push(-1);    // for corner element
-		int ans[] = new int[n];
-		// traverse from R-->L for NSE
-		for(int i=n-1;i>=0;i--)
-			{   // compare peek() with current element
-			    // if peek() is smaller --> store peek() and push
-				if(s.peek()<arr[i]){
-					ans[i] = s.peek();
-					s.push(arr[i]);
-				}else{
-				// if peek is greater --> pop() until peek() is smaller    
-					while(s.peek()>=arr[i]){
-						s.pop();
-					}
-					// store peek() and push
-					ans[i] = s.peek();
-					s.push(arr[i]);
-				}
-			}
-			return ans;
+	    s.push(-1);    // for corner element
+	    int ans[] = new int[n];
+	    // traverse from R-->L for NSE
+	    for(int i=n-1;i>=0;i--)
+	    {   // compare peek() with current element
+	        // if peek() is smaller --> store peek() and push
+	        if(s.peek()<arr[i]){
+	            ans[i] = s.peek();
+	            s.push(arr[i]);
+	        }else{
+	            // if peek is greater --> pop() until peek() is smaller   
+	            while(s.peek()>=arr[i]){
+	                s.pop();
+	            }
+	            // store peek() and push
+	            ans[i] = s.peek();
+	            s.push(arr[i]);
+	        }
+	    }
+	    return ans;
 	} 
 }
